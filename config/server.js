414aed8454194c016a8dab41e65ffda20074f3f6
Gregory Rodriguez <glrd4726ml@gmail.com>
2023-02-20 17:28:19 -0300

    Creacion de servidor de escucha para  apiRest

--- config/server.js
+++ config/server.js
@@ -1,18 +1,20 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+require('dotenv').config(); 
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
 
 const port = process.env.PORT || 3901;
 
+
 const server = () => {
-    if(app.listen(port, () => {})){
-        console.log("Servidor corriendo en el puerto: "+port)
-      }else{
-        console.log("No se pudo conectar al servidor")
-      }
-} 
+    console.log('Iniciando servidor en el puerto: '+port)
+    if(app.listen(port, ()=>{})){
+        console.log('Servidor escuchando por el puerto: '+port)
+    }else{
+        console.log('Servidor no conectado')
+    }
+}
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const server = require('./config/server')
-console.log("Iniciando App de Node");
-const app = express();
-app.use(cors());
+const server = require('./config/server');
+console.log('Iniciando Api Rest');
 
-//Cargando Servidor
+//importando Servidor
 server();
