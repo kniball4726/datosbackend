e39c61d6784b1d4b57883e495ab885dbaaf9a3ec
Gregory Trabajo <glrd4726ml@gmail.com>
2023-02-24 14:19:57 -0300

    Iniciando Api Rest NodeJs

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
 const express = require("express");
-const server = require("./config/server");
-const database = require('./config/database');
+const cors = require('cors');
+require ('dotenv').config();
+//const server = require("./config/server");
+//const database = require('./config/database');
 
 const app = express();
-
+app.use(cors());
 console.log('Iniciando Api Rest | Starting ApiRest');
 
 //Conectando Servidor
 //coneccting server
-server();
+//server();
 
 //Conectando Base de datos
 //connecting database
-database();
+//database();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+//rutas
 
+app.get('/probando', (req, res) => {
+    console.log('Se ha ejecutado endpoint de prueba')
+    return res.status(200).send(`
+       <div>
+        <h1>Probando rutas NodeJs</h1>
+        <p>Creando API REST con NODEJS</p>
+            <ul>
+                <li>Home</li>
+                <li>Nosotros</li>
+                <li>Ubicanos</li>
+            </ul>
+       </div>
+    
+    `)
+});
+
+//Creando servidor
+const port = process.env.PORT || 3901;
+
+console.log('Iniciando Servidor')
+
+if (app.listen(port, () =>{})){
+        console.log('Servidor escuchando por el puerto: '+port)
+    }else{
+        console.log('Servidor no conectado')
+    }
 
 //rutas
-//const rutasProductos = require("./routes/productos.js");
-
-//app.use("/api", rutasProductos);
-
-app.get("/prueba", (req, res)=>{
-    console.log("Endpoint de prueba")
-    return res.status(200).send("Hola");
-    
-})
